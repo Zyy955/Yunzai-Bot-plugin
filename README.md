71a95873ad5eeb3e92b99842ff6c4fab84f2aa74
Zyy955 <1072411694@qq.com>
2023-08-17 04:04:10 +0800

    添加`同时使用星铁插件面板.js`

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
-所有插件的下载连接在`Yunzai-bot根目录执行`，也可以自行查看源码后保存下载
+### 所有插件的下载连接在`Yunzai-bot根目录执行`，也可以自行查看源码后保存下载
 
-### 设置主人
+## 同时使用星铁插件和喵喵插件
+
+目前只兼容了`*更新面板`、`#星铁更新面板`、`*希儿面板`、`#星铁希儿面板`  
+
+！！！并没有只有希儿能用！上面是举个例子！
+
+默认关闭星铁插件，请下载插件后开启使用~
+
+就5个指令...自己琢磨吧~！
+```
+#插件面板状态
+#星铁插件面板开启
+#星铁插件面板关闭
+#喵喵插件面板开启
+#喵喵插件面板关闭
+```
+
+##### 下载
+Github：
+```
+curl -o "./plugins/example/SetMaster.js" "https://raw.githubusercontent.com/Zyy955/Yunzai-Bot-plugin/main/StarRail.js"
+```
+Gitee：
+```
+curl -o "./plugins/example/SetMaster.js" "https://gitee.com/Zyy955/Yunzai-Bot-plugin/raw/main/StarRail.js"
+```
+
+
+## 设置主人
 
 为`Yunzai-Bot`设计，适用于`QQGuild-Plugin`
 
@@ -20,8 +48,28 @@ Gitee：
 curl -o "./plugins/example/SetMaster.js" "https://gitee.com/Zyy955/Yunzai-Bot-plugin/raw/main/SetMaster.js"
 ```
 
+## 无需登录QQ启动机器人
 
-### 踢
+搭配[QQGuild-plugin](https://gitee.com/Zyy955/QQGuild-plugin)插件启用可无需登录QQ使用频道
+
+##### 使用方法
+
+下载完成后，使用`node apps`启动即可~
+
+如果想使用原来的，使用`node app`启动即可~
+
+##### 下载
+Github：
+```
+curl -o "./apps.js" "https://raw.githubusercontent.com/Zyy955/Yunzai-Bot-plugin/main/apps.js"
+```
+
+Gitee：
+```
+curl -o "./apps.js" "https://gitee.com/Zyy955/Yunzai-Bot-plugin/raw/main/apps.js"
+```
+
+## 踢
 
 - 暂时只支持以下几种场景
 - 喵崽的QQ群 私聊
@@ -46,25 +94,4 @@ curl -o "./plugins/example/kick.js" "https://raw.githubusercontent.com/Zyy955/Yu
 Gitee：
 ```
 curl -o "./plugins/example/kick.js" "https://gitee.com/Zyy955/Yunzai-Bot-plugin/raw/main/kick.js"
-```
-
-### 无需登录QQ启动机器人
-
-搭配[QQGuild-plugin](https://gitee.com/Zyy955/QQGuild-plugin)插件启用可无需登录QQ使用频道
-
-##### 使用方法
-
-下载完成后，使用`node apps`启动即可~
-
-如果想使用原来的，使用`node app`启动即可~
-
-##### 下载
-Github：
-```
-curl -o "./apps.js" "https://raw.githubusercontent.com/Zyy955/Yunzai-Bot-plugin/main/apps.js"
-```
-
-Gitee：
-```
-curl -o "./apps.js" "https://gitee.com/Zyy955/Yunzai-Bot-plugin/raw/main/apps.js"
 ```
---
+++ StarRail.js
@@ -0,0 +1,169 @@
+import fs from 'fs'
+import moment from 'moment'
+
+let SR = {
+    miao: true,
+    sr: false
+}
+
+export class StarRail extends plugin {
+    constructor() {
+        super({
+            name: '星铁面板-兼容版',
+            dsc: '可同时使用miao-plugin和StarRail-plugin',
+            event: 'message',
+            priority: -1000,
+            rule: [
+                {
+                    reg: '^#星铁更新面板.*$',
+                    fnc: 'update'
+                },
+                {
+                    reg: '^#星铁(?!插件)(.+)面板$',
+                    fnc: 'StarRail'
+                },
+                {
+                    reg: '^#(喵喵|星铁)?插件面板(开启|关闭|状态)$',
+                    fnc: 'Sr'
+                }
+            ]
+        })
+    }
+
+    /** 更新全部面板 */
+    async update(e) {
+        if (SR.miao) {
+            ((await import("../miao-plugin/apps/profile/ProfileList.js")).default).refresh(e)
+        }
+
+        if (SR.sr) {
+            try {
+                const panelApi = (await import("../StarRail-plugin/runtime/PanelApi.js")).default
+                const api = await panelApi()
+                const { Panel } = await import("../StarRail-plugin/apps/panel.js")
+                let StarRail = new Panel()
+                StarRail.e = e
+                StarRail.reply = this.reply
+                /** 获取星铁uid */
+                const uid = e?.user.getUid('sr') || ""
+                const data = await StarRail.getPanelData(uid, false)
+                let renderData = {
+                    api: api.split('/')[2],
+                    uid,
+                    data,
+                    type: 'update'
+                }
+                /** 渲染数据 */
+                await renderCard(e, renderData)
+                return false
+            } catch (error) {
+                logger.error('SR-panelApi', error)
+                return e.reply(error.message)
+            }
+        }
+    }
+
+    /** 查看角色详细面板 */
+    async StarRail(e) {
+        if (SR.miao) {
+            ((await import("../miao-plugin/apps/profile/ProfileDetail.js")).default).detail(e)
+        }
+
+        if (SR.sr) {
+            try {
+                const panelApi = (await import("../StarRail-plugin/runtime/PanelApi.js")).default
+                const api = await panelApi()
+                const { Panel } = await import("../StarRail-plugin/apps/panel.js")
+                let StarRail = new Panel()
+                StarRail.e = e
+                StarRail.reply = this.reply
+                /** 获取星铁uid */
+                const uid = e?.user.getUid('sr') || ""
+                /** 获取角色名称 */
+                const charName = e.msg.replace(/#|星铁|面板|面版/g, "")
+                const { pluginRoot } = await import("../StarRail-plugin/utils/path.js")
+
+                /** 引入遗器地址数据 */
+                const relicsPathData = readJson('resources/panel/data/relics.json', pluginRoot)
+                // 引入角色数据
+                const charData = readJson('resources/panel/data/character.json', pluginRoot)
+                let data = await StarRail.getCharData(charName, uid, e)
+                data.uid = uid
+                data.api = api.split('/')[2]
+                data.charpath = charData[data.avatarId].path
+                data.relics.forEach((item, i) => {
+                    data.relics[i].path = relicsPathData[item.id]?.icon
+                })
+                // 行迹
+                data.behaviorList = StarRail.handleBehaviorList(data.behaviorList)
+                // 面板图
+                data.charImage = StarRail.getCharImage(data.name, data.avatarId)
+
+                logger.debug(`${e.logFnc} 面板图:`, data.charImage)
+
+                const runtimeRender = (await import('../StarRail-plugin/common/runtimeRender.js')).default
+                let msgId = await runtimeRender(e, '/panel/new_panel.html', data, { retType: 'msgId', cale: 1.6 })
+                msgId && redis.setEx(`STAR_RAILWAY:panelOrigImg:${msgId.message_id}`, 60 * 60, data.charImage)
+            } catch (error) {
+                logger.error('SR-panelApi', error)
+                return await e.reply(error.message)
+            }
+        }
+    }
+    async Sr(e) {
+        const fileurl = import.meta.url
+        const sr_name = fileurl.substring(fileurl.lastIndexOf('/') + 1).split('?')[0]
+        const _path = process.cwd() + "/plugins"
+        let cfg = fs.readFileSync(_path + `/example/${sr_name}`, 'utf8')
+        const msg = e.msg
+        if (/喵喵/.test(msg)) {
+            if (/开启/.test(msg) && !SR.miao) {
+                SR.miao = true
+                cfg = cfg.replace('miao: false', 'miao: true')
+            } else if (/关闭/.test(msg) && SR.miao) {
+                SR.miao = false
+                cfg = cfg.replace('miao: true', 'miao: false')
+            }
+        } else if (/星铁/.test(msg)) {
+            if (!fs.existsSync(_path + "/StarRail-plugin"))
+                return e.reply("....死开，你有这个插件？")
+            if (/开启/.test(msg) && !SR.sr) {
+                SR.sr = true
+                cfg = cfg.replace('sr: false', 'sr: true')
+            } else if (/关闭/.test(msg) && SR.sr) {
+                SR.sr = false
+                cfg = cfg.replace('sr: true', 'sr: false')
+            }
+        }
+        await fs.promises.writeFile(_path + `/example/${sr_name}`, cfg, 'utf8')
+
+        const miao = `喵喵：${SR.miao === true ? "开启" : "关闭"}`
+        const sr_ = `星铁：${SR.sr === true ? "开启" : "关闭"}`
+        e.reply(`当前状态：\n${miao}\n${sr_}`)
+    }
+}
+
+/** 渲染数据 */
+async function renderCard(e, data) {
+    let renderData = {
+        time: moment().format('YYYY-MM-DD HH:mm:ss dddd'),
+        userName: e.sender.card || e.sender.nickname,
+        ...data
+    }
+    const runtimeRender = (await import('../StarRail-plugin/common/runtimeRender.js')).default
+    await runtimeRender(e, '/panel/new_card.html', renderData, {
+        scale: 1.6
+    })
+}
+
+/** 读取JSON文件 */
+function readJson(file, root) {
+    if (fs.existsSync(`${root}/${file}`)) {
+        try {
+            return JSON.parse(fs.readFileSync(`${root}/${file}`, 'utf8'))
+        } catch (e) {
+            logger.error(e)
+        }
+    }
+    return {}
+}
