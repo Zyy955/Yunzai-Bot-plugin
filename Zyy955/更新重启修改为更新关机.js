0efc50fb3ab3ca22a254bdcdf735a18b78df6d2e
Zyy955 <1072411694@qq.com>
2023-09-17 16:04:29 +0800

    更新 更新重启修改为更新关机.js

--- Zyy955/更新重启修改为更新关机.js
+++ Zyy955/更新重启修改为更新关机.js
@@ -3,6 +3,6 @@
 import { update } from "../other/update.js"
 
 update.prototype.restart = async function () {
-    this.e.reply("哒哒哒，你的重启被我吃啦~现在开始关机哦^_^")
+    await this.e.reply("哒哒哒，你的重启被我吃啦~现在开始关机哦^_^")
     return process.exit()
 }
