7120b8ec054836952f6a23931245249abc3989e8
Zyy955 <1072411694@qq.com>
2023-12-04 14:36:48 +0800

    更新 AllGroupList.js

--- AllGroupList.js
+++ AllGroupList.js
@@ -1,4 +1,11 @@
+/**
+ * 仓库：https://github.com/Zyy955/Yunzai-Bot-plugin
+ * gitee：https://gitee.com/Zyy955/Yunzai-Bot-plugin
+ */
 import { Cfg } from '#miao'
+
+/** 设置显示排名数量，不推荐过大，默认100 */
+const num = 100
 
 const ProfileRank = (await import("../miao-plugin/models/ProfileRank.js")).default
 ProfileRank.getGroupUidList = async function (groupId, charId, type = 'mark') {
@@ -10,15 +17,24 @@ ProfileRank.getGroupUidList = async function (groupId, charId, type = 'mark') {
         return parts[2]
     })
 
+    let promises = groupList.map(i => redis.zRangeWithScores(`miao:rank:${i}:${type}:${charId}`, -`${number}`, -1))
+    let results = await Promise.all(promises)
+
     let uids = []
-    for (let i of groupList) {
-        uids.push(...await redis.zRangeWithScores(`miao:rank:${i}:${type}:${charId}`, -`${number}`, -1))
+    for (let result of results) {
+        uids.push(...result)
     }
     /** 去重 */
     uids = Array.from(new Set(uids.map(JSON.stringify))).map(JSON.parse)
+    /** 二次去重 */
+    uids = uids.filter((item, index, self) =>
+        index === self.findIndex((t) => (
+            t.value === item.value && t.score === item.score
+        ))
+    )
     /** 重新排序 */
     uids.sort((a, b) => b.score - a.score)
     /** 只取100个 */
-    if (uids.length > 100) uids.splice(100)
+    if (uids.length > Number(num)) uids.splice(Number(num))
     return uids ? uids.reverse() : false
 }
